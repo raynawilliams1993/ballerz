a4f87abb536c574360f167226c445588e17cf583
rayna williams <Raynawilliams123@gmail.com>
2020-03-01 17:12:19 -0700

    got the backend Authentication files imported and setup, just need help with MONGODBURI

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const passport = require("passport");
+const users = require("./routes/api/users");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -8,15 +9,25 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
+app.use("/api/users", users);
+
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ballerz");
+ { useUnifiedTopology: true };
 
 // Start the API server
 app.listen(PORT, function() {
