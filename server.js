5e4197ef004d005bc17069303baf7c20c391a662
rayna williams <Raynawilliams123@gmail.com>
2020-03-04 10:25:18 -0700

    changed the server

--- server.js
+++ server.js
@@ -1,35 +1,40 @@
 const express = require("express");
-const passport = require("passport");
-const users = require("./routes/api/users");
-const mongoose = require("mongoose");
-const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const colors = require("colors");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const session = require("express-session");
+const passport = require("passport");
+const logger = require("morgan");
+const flash = require('connect-flash');
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Passport middleware
+app.use(logger("dev"));
+app.use(flash())
+app.use(express.static("public"));
+app.use(session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+}));
 app.use(passport.initialize());
+app.use(passport.session());
 
-// Passport config
-require("./config/passport")(passport);
 
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
-// Add routes, both API and view
+
 app.use(routes);
-app.use("../routes/api/users", users);
 
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ballerz");
- { useUnifiedTopology: true };
-
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/react-auth-simple", { useNewUrlParser: true }, function(err) {
+    if (err) throw err;
+    console.log(`mongoose connection successful`.yellow);
+    app.listen(PORT, (err)=> {
+        if (err) throw err;
+        console.log(`connected on port ${PORT}`.cyan)
+    });
 });
