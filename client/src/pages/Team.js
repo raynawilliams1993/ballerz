b9a0b6c567fafab4472817fa7ee11553a5739378
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-03-05 14:32:53 -0700

    got my data displayed on my Teams page

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Table } from "reactstrap"
+import Teams from "../components/Teams.json"
 
-function Team (){
-    return(
-        <p> Team</p>
+function Team(props) {
+    return (
+        <Table>
+            <thead>
+                <tr>
+                    <th>Teamid</th>
+                    <th>Img</th>
+
+                    <th>ABRV</th>
+                    <th>City</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Teams.map(Teams => (
+                    <tr key={Teams.teamId}>
+                        <td scope="row">{Teams.teamId}</td>
+                        <td>
+                            <img src={Teams.img} height="50px" width="50px"></img>
+
+                        </td>
+                        <td>{Teams.ABRV}</td>
+                        <td>{Teams.city}</td>
+                    </tr>
+                ))}
+            </tbody>
+
+        </Table>
     )
+
 }
 
 export default Team;
+{/* <tbody>
+{props.Teams.map(Teams =>(
+   
+        <td/
+  <span onClick={() => props.PastYear(props.id)} className="remove">
+    𝘅
+  </span>
+))}
+
+
+</tbody> */}
